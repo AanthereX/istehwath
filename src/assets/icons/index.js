ea86019193f39e4519567b46d8680ceea39bd503
Agha Hamza <106681004+AanthereX@users.noreply.github.com>
2025-01-27 15:52:07 +0500

    feat: upload files

---
+++ src/assets/icons/index.js
@@ -0,0 +1,139 @@
+/** @format */
+
+import checkIcon from "./checkIcon.svg";
+import buyerIcon from "./buyerIcon.svg";
+import sellerIcon from "./sellerIcon.svg";
+import forgetPasswordKeyIcon from "./forgetPasswordKeyIcon.svg";
+import mailBoxIcon from "./mainBoxIcon.svg";
+import successIcon from "./successIcon.svg";
+import languageIcon from "./languageIcon.svg";
+import nextIcon from "./nextIcon.svg";
+import backIcon from "./backIcon.svg";
+import lessIcon from "./lessIcon.svg";
+import lessIconBlack from "./lessIconBlack.svg";
+import searchIcon from "./searchIcon.svg";
+import blueCheck from "./blueCheck.svg";
+import yellowCheck from "./yellowCheck.svg";
+import location from "./location.svg";
+import blackCheck from "./blackCheck.svg";
+import favorite from "./favorite.svg";
+import dropdown from "./dropdown.svg";
+import dollar from "./dollar.svg";
+import graph from "./graph.svg";
+import goldGraph from "./graphGold.svg";
+import buyerRequestCountImg from "./buyerRequestCountImg.svg";
+import filter from "./filter.svg";
+import filterGold from "./filterGold.svg";
+import check from "./check.svg";
+import checkWhite from "./checkWhite.svg";
+import plus from "./plus.svg";
+import add from "./add.svg";
+import checkBox from "./checkBox.svg";
+import rejectIcon from "./rejectIcon.svg";
+import calender from "./calender.svg";
+import info from "./info.svg";
+import crossIcon from "./crossIcon.svg";
+import successToastIcon from "./successToastIcon.svg";
+import saudiFlag from "./saudiFlag.png";
+import successTickIcon from "./successTickIcon.svg";
+import whatsAppIcon from "./whatsappIcon.svg";
+import requestAwaitingIcon from "./requestAwaitingIcon.svg";
+import requestUnavailableIcon from "./requestUnavailableIcon.svg";
+import notifyBellWithDot from "./notify.svg";
+import notifyOnlyBell from "./notifyWithOnlyBell.svg";
+import notifyOnlyBellGold from "./notifyWithOnlyBellGold.svg";
+import dotsMenu from "./dotsMenu.svg";
+import uploadIcon from "./uploadIcon.svg";
+import deleteTag from "./deleteTag.svg";
+import addIcon from "./addIcon.svg";
+import DocFileIcon from "./docIcon.png";
+import PdfFileIcon from "./pdfIcon.png";
+import Mp4FileIcon from "./mp4Icon.png";
+import SilverBagde from "./silverBadge.svg";
+import downloadIcon from "./downloadIcon.svg";
+import notificationTickIcon from "./notificationTickIcon.svg";
+import notificationCrossIcon from "./notificationCrossIcon.svg";
+import notificationInitiatedIcon from "./notificationInitiatedIcon.svg";
+import downArrowSvg from "./downArrowSvg.svg";
+import sortByIcon from "./sortByIcon.svg";
+import arrowRight from "./arrowRight.svg";
+import instaGramSvgIcon from "./instaGramColoredIcon.svg";
+import copyIconSvgIconFilled from "./copyIconColoredIconFilled.svg";
+import copyIconSvgIconOutline from "./copyIconColoredIconOutline.svg";
+import checkNewIcon from "./checkNewIcon.svg";
+import arrow from "./arrow.svg";
+import infoIconBlack from "./infoIconBlack.svg";
+import deleteIconWithBg from "./deleteIconWithBg.svg";
+import dollarBall from "./dollarBall.svg";
+import dollarBallGold from "./dollarBallGold.svg";
+import plusgold from "./plusgold.svg";
+
+export const Icons = {
+  checkIcon,
+  buyerIcon,
+  sellerIcon,
+  forgetPasswordKeyIcon,
+  mailBoxIcon,
+  successIcon,
+  languageIcon,
+  nextIcon,
+  backIcon,
+  lessIcon,
+  searchIcon,
+  blueCheck,
+  yellowCheck,
+  location,
+  blackCheck,
+  favorite,
+  dollar,
+  graph,
+  dropdown,
+  filter,
+  check,
+  checkWhite,
+  plus,
+  add,
+  checkBox,
+  rejectIcon,
+  calender,
+  info,
+  crossIcon,
+  successToastIcon,
+  saudiFlag,
+  successTickIcon,
+  notificationTickIcon,
+  whatsAppIcon,
+  requestAwaitingIcon,
+  requestUnavailableIcon,
+  notifyBellWithDot,
+  notifyOnlyBell,
+  notifyOnlyBellGold,
+  dotsMenu,
+  uploadIcon,
+  deleteTag,
+  addIcon,
+  DocFileIcon,
+  PdfFileIcon,
+  Mp4FileIcon,
+  SilverBagde,
+  lessIconBlack,
+  downloadIcon,
+  notificationCrossIcon,
+  notificationInitiatedIcon,
+  downArrowSvg,
+  buyerRequestCountImg,
+  sortByIcon,
+  arrowRight,
+  instaGramSvgIcon,
+  copyIconSvgIconFilled,
+  copyIconSvgIconOutline,
+  checkNewIcon,
+  arrow,
+  infoIconBlack,
+  deleteIconWithBg,
+  filterGold,
+  goldGraph,
+  dollarBall,
+  dollarBallGold,
+  plusgold,
+};
---
+++ src/assets/images/index.js
@@ -0,0 +1,61 @@
+/** @format */
+
+import istehwathLogo from "./logo.svg";
+import logo from "./logo.svg";
+import searchbg from "./searchbg.svg";
+import redirect from "./redirect.svg";
+import nexttab from "./nexttab.svg";
+import nextgold from "./nextgold.svg";
+import todo from "./todo.svg";
+import cancel from "./cancel.svg";
+import tasklist from "./tasklist.svg";
+import remove from "./remove.svg";
+import user from "./user.svg";
+import confirm from "./confirm.svg";
+import visa from "./visa.svg";
+import userImg from "./userImg.svg";
+import saudiFlag from "./saudiFlag.svg";
+import defaultAvatar from "./defaultAvatar.png";
+import marketPlaceCircles from "./marketPlaceCircles.svg";
+import marketPlaceCirclesGold from "./marketPlaceCirclesGold.svg";
+import UploadImg from "./uploadlogo.svg";
+import mapImage from "./map-img.png";
+import verifyStartupImg from "./verifyStartupImage.svg";
+import noSearchFoundImg from "./nosearchfound.png";
+import noSearchFoundGold from "./noSearchFoundGold.png";
+import attachmentImg from "./attachmentIcon.svg";
+import subsUpgradeSvg from "./upgradeSubsIcon.svg";
+import Google from "./google.svg";
+import AppleLogo from "./appleLogo.svg";
+import Logo from "./logo.svg";
+
+export const Images = {
+  istehwathLogo,
+  logo,
+  searchbg,
+  redirect,
+  nexttab,
+  todo,
+  defaultAvatar,
+  cancel,
+  tasklist,
+  remove,
+  user,
+  confirm,
+  visa,
+  userImg,
+  saudiFlag,
+  UploadImg,
+  marketPlaceCircles,
+  marketPlaceCirclesGold,
+  mapImage,
+  verifyStartupImg,
+  noSearchFoundImg,
+  attachmentImg,
+  noSearchFoundGold,
+  subsUpgradeSvg,
+  Google,
+  Logo,
+  AppleLogo,
+  nextgold,
+};
